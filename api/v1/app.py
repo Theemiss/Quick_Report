5e5aabd37c42047d08f3c71f4471936c4fca06d8
Theemiss <midinfotn401@gmail.com>
2021-06-06 21:25:06 +0000

    update

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,9 +6,10 @@ from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_restful_swagger import swagger
 from dotenv import dotenv_values
+from flask_apispec.extension import FlaskApiSpec
 config = dotenv_values('.env')
 app = Flask(__name__)
-api = swagger.docs(Api(app), apiVersion='0.1')
+api = swagger.docs(Api(app), apiVersion='2.8')
 jwt = JWTManager(app)
 
 
