d4c0989b8cbe1be45896cc29b70e26ce886ff50a
Themis <midinfotn401@gmail.com>
2021-06-09 16:23:13 +0100

    update

--- web_app/src/components/login/login.js
+++ web_app/src/components/login/login.js
@@ -9,7 +9,7 @@ import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
-import { Link, useHistory } from "react-router-dom"
+import { Link } from "react-router-dom"
 import PropTypes from 'prop-types';
 
 
