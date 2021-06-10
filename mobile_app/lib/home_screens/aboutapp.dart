c550932ed17e970829c979141c01b5dba1cffe1d
aminbnd <2212@holbertonschool.com>
2021-06-10 17:43:44 +0200

    pushing changes in main branch

--- mobile_app/lib/home_screens/aboutapp.dart
+++ mobile_app/lib/home_screens/aboutapp.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:more_pro_ui_qr/Navigation/navigation_drawer.dart';
 
@@ -5,12 +7,146 @@ class AboutTheApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: true,
       drawer: NavigationDrawerWidget(),
       appBar: AppBar(
+        toolbarHeight: 70,
         title: Text('About Quick Report'),
         centerTitle: true,
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.vertical(
+                bottom: Radius.circular(25),
+              ),
+              gradient: LinearGradient(
+                colors: [Colors.redAccent, Colors.blueAccent],
+                begin: Alignment.bottomRight,
+                end: Alignment.topLeft,
+              )),
+        ),
+        elevation: 30,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(25),
+          ),
+        ),
       ),
-      // body: ,
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            SizedBox(height: 80),
+            Container(
+              height: 150,
+              child: Center(
+                child: Text('Welcom to Quick Report',
+                    style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blueAccent)),
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Vesrion:',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            Container(child: Text('1.0.0')),
+            SizedBox(height: 50),
+            Text(
+              'About Quick Report',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            Container(
+              child: Text(
+                  'Have you ever thought about how long it will take to fill and transmit papers about traffic accidents in Tunisia? The answer is  DAYS … and probably WEEKS\n\nThis app finds a solution for that.\n\nActually, the current way to fill a traffic accident report to insurance companies could last a long time, especially with the high number of car crashes that make the procedure heavy. Besides, there is a high error occurrence risk.\n\nHowever, the app cannot be used outside the Tunisian territory or for vehicles insured by foreign insurance companies. Also, the app cannot be used in case of human damage (even slight damage).\n\n'),
+            ),
+            SizedBox(height: 50),
+            Text(
+              'Authors:',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            Column(
+              children: <Widget>[
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Container(
+                    child: Text('Ahmed Belhaj:',
+                        style: TextStyle(fontWeight: FontWeight.bold)),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 15),
+            Column(
+              children: <Widget>[
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Container(
+                    child: Text(
+                      'Software Engineering Student at Holberton School',
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 40),
+            Column(
+              children: <Widget>[
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Container(
+                    child: Text('Mohamed Chedli:',
+                        style: TextStyle(fontWeight: FontWeight.bold)),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 15),
+            Column(
+              children: <Widget>[
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Container(
+                    child: Text(
+                      'Software Engineering Student at Holberton School',
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 40),
+            Column(
+              children: <Widget>[
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Container(
+                    child: Text('Med Amin Bondi:',
+                        style: TextStyle(fontWeight: FontWeight.bold)),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 15),
+            Column(
+              children: <Widget>[
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Container(
+                    child: Text(
+                      'Software Engineering Student at Holberton School',
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 50),
+            // SizedBox(height: 30),
+          ],
+        ),
+      ),
     );
   }
 }
--- mobile_app/lib/home_screens/myprofile.dart
+++ mobile_app/lib/home_screens/myprofile.dart
@@ -12,10 +12,36 @@ class _MyProfileState extends State<MyProfile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: true,
       drawer: NavigationDrawerWidget(),
       appBar: AppBar(
+        toolbarHeight: 70,
         title: Text('My Profile'),
         centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.notifications),
+          ),
+        ],
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.vertical(
+              bottom: Radius.circular(25),
+            ),
+            gradient: LinearGradient(
+              colors: [Colors.redAccent, Colors.blueAccent],
+              begin: Alignment.bottomRight,
+              end: Alignment.topLeft,
+            ),
+          ),
+        ),
+        elevation: 30,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(25),
+          ),
+        ),
       ),
     );
   }
